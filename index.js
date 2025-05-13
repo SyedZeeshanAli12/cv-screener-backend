5888e6b55ef5ff9f788846403ef2627f51da507a
osamacloudpacer <osama.arshed@cloudpacer.com>
2025-05-13 19:45:30 +0500

    add root health route

--- index.js
+++ index.js
@@ -14,3 +14,7 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
+app.get('/', (req, res) => {
+  res.send('Backend is up and running 🚀');
+});
+
